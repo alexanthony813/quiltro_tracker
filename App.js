57324069af26e7966bc91c8e59f30a47b5beb7fa
Alexander Anthony <alexanderanthony@Alexanders-MacBook-Pro.local>
2023-06-20 19:31:45 -0400

    Start messaging

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Alert } from 'react-native'
 import { TailwindProvider } from 'tailwindcss-react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import AppLoading from 'expo-app-loading'
@@ -10,11 +11,57 @@ import AuthNavigator from './navigation/AuthNavigator'
 import AuthContext from './auth/context'
 import authStorage from './auth/storage'
 import { navigationRef } from './navigation/rootNavigation'
+import { StatusBar } from 'expo-status-bar'
+
+const requestUserPermission = async () => {
+  const authStatus = await messaging().requestUserPermission()
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL
+
+  console.dir(authStatus)
+}
 
 export default function App() {
   const [_, setUser] = useState()
   const [error, setError] = useState()
   const [isReady, setIsReady] = useState(false)
+  const []
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      // return token 
+      messaging().getToken().then(token => {
+        console.dir(token)
+      })
+    } else {
+      console.dir(`Failed token status ${authStatus}`)
+    }
+
+    messaging().getInitialNotification().then(remoteMessage => {
+      if (remoteMessage) {
+        console.dir(`Notification caused app to open from quit state: ${remoteMessage.notification}`)
+      //  setInitialRoute(remoteMessage.date.type)  // not needed? test
+      }
+      // setLoading(false) // not needed? test
+    })
+
+    messaging().onNotificationOpenedApp(async remoteMessage => {
+      console.log(`Notification caused app to open from background state: ${remoteMessage.notification}`)
+      // navigationRef.navigate(remoteMessage.data.type) // not needed? test
+
+    })
+
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.dir(`Message handled in the background! ${remoteMessage}`)
+    })
+
+    const unsubscribe = messaging().onMessage(aysnc remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+    })
+
+    return unsubscribe
+  }, [])
 
   const restoreUser = async () => {
     let user = await authStorage.getUser()
@@ -32,6 +79,13 @@ export default function App() {
       />
     )
   }
+
+  const messagingComponent = (
+    <View className="flex justify-center align-center">
+      <Text>FCM Basic POC</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
